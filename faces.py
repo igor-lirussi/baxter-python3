051b2d0a476073b698c566659d3513e92f0ea0dd
igor-lirussi <igor.lirussi@studio.unibo.it>
2023-09-26 17:31:25 +0300

    fixed indentation in faces

--- faces.py
+++ faces.py
@@ -2,10 +2,10 @@ import cv2
 
 
 def _set_face(robot, face="look_down", activated=True):
-	"""
-	sets the robot face to a specific one 
-	activated is a default true variable that can be passed with a global variable in your code if you want to deactivate the face change all together
-	"""
+    """
+    sets the robot face to a specific one 
+    activated is a default true variable that can be passed with a global variable in your code if you want to deactivate the face change all together
+    """
     if activated:
         path ="./faces/"+str(face)+".jpg"
         print(path)
@@ -14,8 +14,8 @@ def _set_face(robot, face="look_down", activated=True):
         robot._set_display_data(image)
 
 def _set_look(robot, look_direction="down", activated=True):
-	"""
-	sets the robot looking direction to a specific one (down, up, left, right, frontal, left_up,...)
-	activated is a default true variable that can be passed with a global variable in your code if you want to deactivate the face change all together
-	"""
-	_set_face(robot,"look_"+look_direction,activated)
+    """
+    sets the robot looking direction to a specific one (down, up, left, right, frontal, left_up,...)
+    activated is a default true variable that can be passed with a global variable in your code if you want to deactivate the face change all together
+    """
+    _set_face(robot,"look_"+look_direction,activated)
