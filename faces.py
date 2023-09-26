b37db46f666fd5ea540fc02a9fa0559ab8394a30
igor-lirussi <igor.lirussi@studio.unibo.it>
2023-09-26 17:45:10 +0300

    fixed faces path

--- faces.py
+++ faces.py
@@ -1,21 +1,24 @@
 import cv2
 
 
-def _set_face(robot, face="look_down", activated=True):
+def _set_face(robot, face="look_down", activated=True, path="./baxter-python3/faces/"):
     """
     sets the robot face to a specific one 
     activated is a default true variable that can be passed with a global variable in your code if you want to deactivate the face change all together
     """
     if activated:
-        path ="./faces/"+str(face)+".jpg"
+        path=path+str(face)+".jpg"
         print(path)
         img = cv2.imread(path)
-        image = cv2.resize(img, (1024,600))
-        robot._set_display_data(image)
+        if img != None:
+            image = cv2.resize(img, (1024,600))
+            robot._set_display_data(image)
+        else:
+            print("Face file not found!")
 
-def _set_look(robot, look_direction="down", activated=True):
+def _set_look(robot, look_direction="down", activated=True, path="./baxter-python3/faces/"):
     """
     sets the robot looking direction to a specific one (down, up, left, right, frontal, left_up,...)
     activated is a default true variable that can be passed with a global variable in your code if you want to deactivate the face change all together
     """
-    _set_face(robot,"look_"+look_direction,activated)
+    _set_face(robot,"look_"+look_direction,activated,path)
